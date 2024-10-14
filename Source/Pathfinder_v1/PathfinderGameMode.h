92ee803e34ac3de6073312ce0dd1b07f3ceffb10
kaitlyn-dodds <sub@kadodds.com>
2024-10-13 20:41:05 -0600

    adding pause menu, custom game modes, controller

---
+++ Source/Pathfinder_v1/PathfinderGameMode.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "PathfinderGameMode.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PATHFINDER_V1_API APathfinderGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
---
+++ Source/Pathfinder_v1/PathfinderPlayerController.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/PlayerController.h"
+#include "PathfinderPlayerController.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PATHFINDER_V1_API APathfinderPlayerController : public APlayerController
+{
+	GENERATED_BODY()
+	
+};
